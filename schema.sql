db852fec74bece0f461619b70f809d292ea7b4fb
ysabellakristine <167633653+ysabellakristine@users.noreply.github.com>
2024-10-04 19:08:09 +0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,8 @@
 -- NOT NULL = ensures that user must fill this in
 -- UNIQUE = no two email can be the same
+-- changed to create table if not exists since i create the database within the dbConfig file
 
-CREATE TABLE Users (
+CREATE TABLE IF NOT EXISTS Users (
     User_id INTEGER PRIMARY KEY,
     Username VARCHAR(50) NOT NULL,
     Password VARCHAR(50) NOT NULL,
@@ -9,7 +10,7 @@ CREATE TABLE Users (
     Address TEXT
 );
 
-CREATE TABLE Books (
+CREATE TABLE IF NOT EXISTS Books (
     Book_id INTEGER PRIMARY KEY,
     Title VARCHAR(500),
     Author VARCHAR(250),
@@ -20,23 +21,28 @@ CREATE TABLE Books (
     Stock_Available INTEGER
 );
 
-CREATE TABLE Checkout (
+CREATE TABLE IF NOT EXISTS Checkout (
     Checkout_id INTEGER PRIMARY KEY,
     User_id INTEGER,
     Checkout_date DATE,
     Return_date DATE,
     Delivery_id INTEGER,
     Return_delivery_id INTEGER,
-    Expected_delivery_date DATE
+    Expected_delivery_date DATE,
+    FOREIGN KEY (User_id) REFERENCES Users(User_id),  -- Foreign key reference to Users
+    FOREIGN KEY (Delivery_id) REFERENCES Deliveries(Delivery_id),  -- Foreign key reference to Deliveries
+    FOREIGN KEY (Return_delivery_id) REFERENCES Deliveries(Delivery_id)  -- Foreign key reference to Deliveries
 );
 
-CREATE Table Checkout_Items (
+CREATE TABLE IF NOT EXISTS Checkout_Items (
     Checkout_item_id INTEGER PRIMARY KEY,
     Checkout_id INTEGER,
-    Book_id INTEGER
+    Book_id INTEGER,
+    FOREIGN KEY (Checkout_id) REFERENCES Checkout(Checkout_id),  -- Foreign key reference to Checkout
+    FOREIGN KEY (Book_id) REFERENCES Books(Book_id)  -- Foreign key reference to Books
 );
 
-CREATE Table Deliveries (
+CREATE TABLE IF NOT EXISTS Deliveries (
     Delivery_id INTEGER PRIMARY KEY,
     Delivery_date DATE,
     Delivery_address TEXT,
@@ -44,11 +50,13 @@ CREATE Table Deliveries (
     Delivery_type TEXT
 );
 
-  CREATE Table Reviews (
+CREATE TABLE IF NOT EXISTS Reviews (
     Review_id INTEGER PRIMARY KEY,
     Book_id INTEGER,
     User_id INTEGER,
     Rating INTEGER,
     Comment TEXT,
-    Review_date date
+    Review_date DATE,
+    FOREIGN KEY (Book_id) REFERENCES Books(Book_id),  -- Foreign key reference to Books
+    FOREIGN KEY (User_id) REFERENCES Users(User_id)  -- Foreign key reference to Users
 );
