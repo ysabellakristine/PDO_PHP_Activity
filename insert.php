efa19ca5e14fa5034ae64886e9da03cf5cc5ef69
ysabellakristine <167633653+ysabellakristine@users.noreply.github.com>
2024-10-04 17:03:05 +0800

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,7 +2,7 @@
 require 'dbConfig.php';                           //uses the dbConfig.php file
 
 try {
-  $con = newPDO($dsn, $user, $password, $options); // creates PDO instance
+  $con = new PDO($dsn, $user, $password, $options); // creates PDO instance
   $sql = file_get_contents('database.sql');       // reads content of the database.sql file
   $conn->exec($sql);                              // executes sql statements within the file
   echo "Ayie na-insert yung SQL file na maayos"; // prints statement once completed
