fe07e6bd11684400a4da36867cad86cf3c899bf9
ysabellakristine <167633653+ysabellakristine@users.noreply.github.com>
2024-10-05 20:05:38 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Page</title>
+    <link rel="stylesheet" href="styles.css"> 
+
+</head>
+    <div class = "container">
+    <h1>（づ￣3￣）づ╭❤️～</h1>
+    <h2>Click the buttons for navigation</h2>
+    <hr>
+    <p> CODE DEMONSTRATION OF FETCH_ALL(). USE PRINT_R(). WITH “&lt;pre&gt;” TAG IN BETWEEN. </p>
+    <form action="fetchall_page.php" method="get">
+        <button type="navigation">Fetchall() DEMO</button>
+    </form>
+    <hr>
+    <p> CODE DEMONSTRATION OF HOW FETCH() IS USED. USE PRINT_R(). WITH “&lt;pre&gt;” TAG IN BETWEEN.  </p>
+    <form action="fetch_page.php" method="get">
+        <button type="alt_navigation">Fetch() DEMO</button>
+    </form>
+    <hr>
+    <p> CODE DEMONSTRATION OF INSERTION OF RECORD TO YOUR DATABASE</p>
+    <form action="insertion_page.php" method="get">
+        <button type="navigation">Insertion of Record</button>
+    </form>
+    <hr>
+    <p> CODE DEMONSTRATION OF DELETION OF RECORD TO YOUR DATABASE </p>
+    <form action="deletion_page.php" method="get">
+        <button type="alt_navigation">Deletion of Record</button>
+    </form>
+    <hr>
+    <p> CODE DEMONSTRATION OF UPDATING OF RECORD FROM YOUR DATABASE</p>
+    <form action="update_page.php" method="get">
+        <button type="navigation">Updating of Record</button>
+    </form>
+    <hr>
+    <p> CODE DEMONSTRATION OF AN SQL QUERY&#39S RESULT SET IS RENDERED ON AN HTML TABLE
+    </p>
+    <form action="query_page.php" method="get">
+        <button type="alt_navigation">SQL Query Demo</button>
+    </form>
+    <hr>
+ 
+</div>
+</body>
+</html>
